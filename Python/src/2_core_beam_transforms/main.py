441cdd5fd59c958521d746c2fa937743e59f548f
polortiz <polortizandreu@gmail.com>
2020-01-29 16:54:39 +0000

    replaced main2.py for main.py

--- Python/src/2_core_beam_transforms/main.py
+++ Python/src/2_core_beam_transforms/main.py
@@ -1,49 +1,23 @@
-from apache_beam.options.pipeline_options import PipelineOptions
-from custom_core_beam_transforms.pardo_transforms import *
-from apache_beam.options.pipeline_options import PipelineOptions
-
-from custom_core_beam_transforms.pardo_transforms import *
+import apache_beam as beam
+from core_beam_pipelines.pardo import ParDoTransformPipeline
+from pipeline_options.custom_pipeline_options import CustomPipelineOptions
 
 
-def pipeline_builder():
-    pipeline_options = PipelineOptions()
-    # ParDos
-    unique_words_pardo_transform = "Unique words ParDo transform." >> beam.ParDo(UniqueWordsParDo())
-    simple_map_pardo_transform = "Simple map ParDo transform" >> beam.ParDo(SimpleMapPardo())
-    element_printer_pardo_transform = beam.ParDo(PrinterParDo())
-
-    # GroupByKey
-    # noinspection PyUnresolvedReferences
-    fixed_window_transform = beam.WindowInto(beam.window.FixedWindows(60))
-    group_by_transform = beam.GroupByKey()
-
-    # Example source
-    input_read_transform = beam.Create([
-        'To be, or not to be: that is the question: ',
-        "Whether 'tis nobler in the mind to suffer ",
-        'The slings and arrows of outrageous fortune, ',
-        'Or to take arms against a sea of troubles, '])
-
-    with beam.Pipeline(options=pipeline_options) as pipeline:
-        output_before_window = (pipeline | input_read_transform
-                                | unique_words_pardo_transform
-                                | simple_map_pardo_transform)
-        without_window = (output_before_window | "Without window" >> element_printer_pardo_transform)
-        applying_window = (output_before_window | fixed_window_transform
-                           | group_by_transform)
-        (applying_window | "Applying window" >> element_printer_pardo_transform)
-
-        def summer(x, y):
-            return x, sum(y)
-
-        # Map has exactly a correlation of 1:1 1 Input will always give 1 output
-        result = (applying_window
-                  | beam.MapTuple(summer)
-                  | "After Reduce " >> beam.Map(print))
+def pipeline_applier_factory(pipeline: beam.pipeline, pipeline_options: CustomPipelineOptions):
+    factory_dict = {
+        "PARDO": ParDoTransformPipeline,  # TODO: Complete with all the classes
+        "GROUPBYKEY": None,
+        "COMBINE": None,
+        "COGROUPBYKEY": None,
+        "FLATTEN": None,
+        "PARTITION": None
+    }
+    return factory_dict[pipeline_options.transform](pipeline, pipeline_options)
 
 
 if __name__ == '__main__':
-    print("YOU ARE RUNNING A BEAUTIFUL PIPELINE, ENJOY ;)")
-    # The objective on this pipeline will be to perform all the Core Beam transforms to explain them
-    # https://beam.apache.org/documentation/programming-guide/#core-beam-transforms
-    pipeline_builder()
+    pipeline_options = CustomPipelineOptions()
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+        pipeline_applier = pipeline_applier_factory(pipeline, pipeline_options)
+        pipeline_applier.pipeline_builder()
+        pipeline.run()
